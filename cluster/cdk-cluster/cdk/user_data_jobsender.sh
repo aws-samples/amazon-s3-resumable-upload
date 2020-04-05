b23bf3a7211284b1d418b00ffa84246f5f1fcb45
huangzbaws <huangzb@amazon.com>
2020-04-05 15:41:43 +0800

    fix cdk userdat reboot start the app
    chmod +x /etc/rc.local

--- cluster/cdk-cluster/cdk/user_data_jobsender.sh
+++ cluster/cdk-cluster/cdk/user_data_jobsender.sh
@@ -9,6 +9,7 @@ chown -R ec2-user:ec2-user amazon-s3-resumable-upload/
 
 # Add to start application
 echo "nohup python3 /home/ec2-user/amazon-s3-resumable-upload/cluster/s3_migration_cluster_jobsender.py &" >> /etc/rc.local
+chmod +x /etc/rc.local
 
 echo "Start python3 s3_migration_cluster_jobsender.py"
 cd amazon-s3-resumable-upload/cluster/  || exit
